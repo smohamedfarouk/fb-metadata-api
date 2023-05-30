d879f8f7769752010d5d7e6ce9069921d2d67e27
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-30 11:26:14 +0100

    Activate applicable rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module FbMetadataApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.active_support.disable_to_s_conversion = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
